86b5afa364ec63d5400c744de83edcb2180d1913
Venkat <venkat@venkat-Inspiron-N5050.(none)>
2013-02-24 01:18:27 +0530

    works on feedback form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   #before_filter :authenticate_user!
+  
   rescue_from Stripe::CardError do |exception|
  render :text=>exception
-end
-   private
+  end
+  private
 
 
 
