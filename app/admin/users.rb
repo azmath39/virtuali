b9042ad4479fa4db31260212bc27c4e8e9c410db
venkat <venkat@venkat-Inspiron-N5050>
2013-03-13 14:05:24 +0530

    changed navabar background

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,8 +3,8 @@ ActiveAdmin.register User do
      member_action :status do
 
      @users = User.all
-    @tours = Tour.all
-    @payments = Payment.all
+     @tours = Tour.all
+     @payments = Payment.all
       #@u = User.find(params[:id])
 
       # This will render app/views/admin/posts/comments.html.erb
