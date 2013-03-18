a09a6a3dc92b1876093e07e25b6ef3dea3b75275
jagadish <mejagadishwer@brilliance-works.com>
2013-03-18 11:05:47 +0530

    fdjhgb

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,24 +6,24 @@ ActiveAdmin.register User do
       f.input :password_confirmation
       f.input :phno
       f.input   "add1"
-    f.input   "add2"
-    f.input   "state"
-    f.input   "city"
-    f.input   "zipcode"
+      f.input   "add2"
+      f.input   "state"
+      f.input   "city"
+      f.input   "zipcode"
     end
     f.actions
   end 
- config.clear_action_items!
-     member_action :status do
+  config.clear_action_items!
+  member_action :status do
 
-     @users = User.all
-     @tours = Tour.all
-     @payments = Payment.find(:all,:order=>'created_at DESC')
-      #@u = User.find(params[:id])
-      @app_size= User::appliction_size
-      # This will render app/views/admin/posts/comments.html.erb
-    end
+    @users = User.all
+    @tours = Tour.all
+    @payments = Payment.find(:all,:order=>'created_at DESC')
+    #@u = User.find(params[:id])
+    @app_size= User::appliction_size
+    # This will render app/views/admin/posts/comments.html.erb
+  end
      
-   menu :label => "Status Report" ,:url=> '/admin/users/status/status'
+  menu :label => "Status Report" ,:url=> '/admin/users/status/status'
    
 end
