ec075aedfdb0bd6c280fb6759e5dece279160792
Venkat <venkat@venkat-Inspiron-N5050.(none)>
2013-04-15 16:56:40 +0530

    single page navigation

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,13 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+Rack::Mime::MIME_TYPES.merge!({
+  ".ogg"     => "application/ogg",
+  ".ogx"     => "application/ogg",
+  ".ogv"     => "videos/ogg",
+  ".oga"     => "audio/ogg",
+  ".mp4"     => "videos/mp4",
+  ".m4v"     => "videos/mp4",
+  ".mp3"     => "audio/mpeg",
+  ".m4a"     => "audio/mpeg"
+})
